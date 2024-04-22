fb041a8cbb31fc6dc195b42eb0255b27466cb450
João Miguel <115197583+joaomiguelg@users.noreply.github.com>
2024-04-22 20:27:25 -0300

    create petDetails

--- src/pages/PetDetails/PetDetails.tsx
+++ src/pages/PetDetails/PetDetails.tsx
@@ -5,14 +5,16 @@ import styles from './PetDetails.module.css'
 import { useQuery } from "@tanstack/react-query";
 import { getPetById } from "../../services/pets/getPetsById";
 import { ImageBase64 } from "../../components/common/ImageBase64/ImageBase64";
+import { Skeleton } from "../../components/common/Skeleton";
 
 export function PetDetails() {
-    const {id} = useParams()
+    const { id } = useParams()
 
-    const {data} = useQuery({queryKey: ['get-pet-by-id', id], queryFn: async () => {
-        return await getPetById(id ?? '')
-    },
-})
+    const { data, isLoading } = useQuery({
+        queryKey: ['get-pet-by-id', id], queryFn: async () => {
+            return await getPetById(id ?? '')
+        },
+    })
 
     return (
 
@@ -20,10 +22,24 @@ export function PetDetails() {
             <div className={styles.container}>
                 <Header showReturn={true} />
                 <main className={styles.content}>
-                    <ImageBase64 src={data?.photo} className={styles.picture} />
-                    <h1>{data?.name}</h1>
-                    <span>Sobre o pet:</span>
-                    <p>{data?.bio}</p>
+                    {
+                        isLoading && <div className={styles.skeleton}>
+                            <Skeleton circle={true} width={200} height={200}/>
+                            <Skeleton width={180} height={24} style={{margin: 16}} />
+                        </div>
+                    }
+                    {
+                        !isLoading && (
+                            <>
+                            <ImageBase64 src={data?.photo} className={styles.picture} />
+                            <h1>{data?.name}</h1>
+                            <span>Sobre o pet:</span>
+                            <p>{data?.bio}</p>
+                            </>
+
+                        )
+                    }
+
                 </main>
             </div>
 
